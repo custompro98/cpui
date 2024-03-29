c4bc6e9583bb6ada82456ecaf1bdc5a434f7724c
custompro98 <mitchjoa@gmail.com>
2024-03-29 12:24:26 -0400

    dist v2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "react-beautiful-timeline",
+      name: "cpui",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
